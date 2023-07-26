96526d373b5f7307d618cf36331c7e3d28fc2a21
Tituz175 <tobititus01@gmail.com>
2023-07-25 22:27:36 -0600

    task 9 done

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -2,59 +2,80 @@
 
 void count_sort(int *array, size_t size, int index);
 
+/**
+ * count_sort -> this function implements the counting sort
+ * algorithm
+ *
+ * @array: the arrays of integers to be sort
+ * @size: the size of the array
+ * @index: The divisor used to extract digits for sorting.
+ * Should be a power of 10 corresponding to the place value
+ * of the digit being sorted.
+ */
+
 void count_sort(int *array, size_t size, int index)
 {
-    unsigned int i, temp;
-    int *count_array, *sorted_array;
+	unsigned int i, temp;
+	int *count_array, *sorted_array;
 
-    count_array = malloc(sizeof(int) * 10);
+	count_array = malloc(sizeof(int) * 10);
 
-    if (count_array == NULL)
-        return;
-    sorted_array = malloc(sizeof(int) * size);
-    if (sorted_array == NULL)
-    {
-        free(count_array);
-        return;
-    }
+	if (count_array == NULL)
+		return;
+	sorted_array = malloc(sizeof(int) * size);
+	if (sorted_array == NULL)
+	{
+		free(count_array);
+		return;
+	}
 
-    for (i = 0; i < 10; i++)
-        count_array[i] = 0;
+	for (i = 0; i < 10; i++)
+		count_array[i] = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        temp = array[i] / index;
-        count_array[temp % 10]++;
-    }
+	for (i = 0; i < size; i++)
+	{
+		temp = array[i] / index;
+		count_array[temp % 10]++;
+	}
 
-    for (i = 1; i < 10; i++)
-        count_array[i] = count_array[i] + count_array[i - 1];
+	for (i = 1; i < 10; i++)
+		count_array[i] = count_array[i] + count_array[i - 1];
 
 
-    for (i = size - 1; (int)i > -1; i--)
-    {
-        temp = array[i] / index;
-        sorted_array[count_array[temp % 10] - 1] = array[i];
-        count_array[temp % 10]--;
-    }
+	for (i = size - 1; (int)i > -1; i--)
+	{
+		temp = array[i] / index;
+		sorted_array[count_array[temp % 10] - 1] = array[i];
+		count_array[temp % 10]--;
+	}
 
-    for (i = 0; i < size; i++)
-        array[i] = sorted_array[i];
+	for (i = 0; i < size; i++)
+		array[i] = sorted_array[i];
 
-    print_array(array, size);
+	print_array(array, size);
 
-    free(sorted_array);
-    free(count_array);
+	free(sorted_array);
+	free(count_array);
 }
+
+/**
+ * radix_sort -> this function implements radix sort algorithm.
+ *
+ * @array: the arrays of integers to be sort
+ * @size: the size of the array
+ */
 
 void radix_sort(int *array, size_t size)
 {
-    unsigned int i;
-    int max_value = array[0];
+	unsigned int i;
+	int max_value = array[0];
 
-    for (i = 1; i < size; i++)
-        max_value = max_value > array[i] ? max_value : array[i];
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 1; max_value / i > 0; i *= 10)
-        count_sort(array, size, i);
+	for (i = 1; i < size; i++)
+		max_value = max_value > array[i] ? max_value : array[i];
+
+	for (i = 1; max_value / i > 0; i *= 10)
+		count_sort(array, size, i);
 }
