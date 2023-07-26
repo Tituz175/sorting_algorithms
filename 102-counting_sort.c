acabdbf69b78fe65e04007fe4d26601054e0fcf3
Tituz175 <tobititus01@gmail.com>
2023-07-25 21:28:31 -0600

    task 6 done

---
+++ 102-counting_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * counting_sort -> this function implements the counting sort
+ * algorithm
+ *
+ * @array: the arrays of integers to be sort
+ * @size: the size of the array
+ */
+
+void counting_sort(int *array, size_t size)
+{
+	unsigned int i, temp, key = array[0];
+	int *count_array, *sorted_array;
+
+	for (i = 1; i < size; i++)
+		key = key > (unsigned int)array[i] ? key : (unsigned int)array[i];
+
+	count_array = malloc(sizeof(int) * key + 1);
+
+	if (count_array == NULL)
+		return;
+	sorted_array = malloc(sizeof(int) * size);
+	if (sorted_array == NULL)
+	{
+		free(count_array);
+		return;
+	}
+
+	for (i = 0; i <= key; i++)
+		count_array[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count_array[array[i]]++;
+
+	for (i = 1; i <= key; i++)
+		count_array[i] = count_array[i] + count_array[i - 1];
+
+	print_array(count_array, key + 1);
+
+	for (i = size - 1; (int)i > -1; i--)
+	{
+		count_array[array[i]] = count_array[array[i]] - 1;
+		temp = count_array[array[i]];
+		sorted_array[temp] = array[i];
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = sorted_array[i];
+
+	free(count_array);
+	free(sorted_array);
+}
--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void quick_sort(int *array, size_t size);
 void lomuto(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT */
